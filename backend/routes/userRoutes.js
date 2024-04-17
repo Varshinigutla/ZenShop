aedac78c9a478be3c7f53eb3abf9372dfafcfb68
Varshinigutla <varshinigutla@gmail.com>
2024-04-17 19:24:39 +0530

    frontend of login and home page

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,7 +16,11 @@ import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 
-router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
+
 
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);

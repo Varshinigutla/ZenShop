61f0f1c5072785b077269e1ee36729c449628c25
neha055 <nehasingh8151@gmail.com>
2024-06-05 00:43:12 +0530

    userorder & orderlist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,6 +20,7 @@ import UserList from './pages/Admin/UserList.jsx';
 import CategoryList from './pages/Admin/CategoryList.jsx';
 import ProductList from './pages/Admin/ProductList.jsx';
 import AllProducts from './pages/Admin/AllProducts.jsx';
+import OrderList from './pages/Admin/OrderList.jsx';
 import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
 import Home from './pages/Home.jsx';
 
@@ -28,6 +29,7 @@ import ProductDetails from './pages/Products/ProductDetails.jsx';
 
 import Cart from "./pages/Cart.jsx";
 import Shop from './pages/Shop.jsx';
+import UserOrder from './pages/User/UserOrder.jsx';
 
 import Shipping from "./pages/Orders/Shipping.jsx";
 import PlaceOrder from './pages/Orders/PlaceOrder.jsx';
@@ -45,6 +47,7 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/shop" element={<Shop />} />
+      <Route path="/user-orders" element={<UserOrder />} />
 
        <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
@@ -59,6 +62,7 @@ const router = createBrowserRouter(
         <Route path="categorylist" element={<CategoryList />} />
         <Route path="productlist" element={<ProductList />} />
         <Route path="allproductslist" element={<AllProducts />} />
+        <Route path="orderlist" element={<OrderList />} />
         <Route path="product/update/:_id" element={<ProductUpdate />} />
        </Route>
     </Route>
